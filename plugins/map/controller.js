eef9c4a39705264259f594234e34d823eaa0648f
insung <libertines11@gmail.com>
2019-02-15 17:21:29 +0900

    merge 재시도

--- plugins/map/controller.js
+++ plugins/map/controller.js
@@ -1,7 +1,7 @@
 function Map($scope, $http, GeolocationService, SpeechService, Focus) {
 	var map = {};
-	map.center = "Seattle, WA"; //default map locaiton
-	map.zoom = 13; //default zoom is 13
+	map.center = "37.4981284,127.028303"; //default map locaiton
+	map.zoom = 16; //default zoom is 16
 
     // Get the current location of the mirror
 	GeolocationService.getLocation({ enableHighAccuracy: true }).then(function (geoposition) {
@@ -19,8 +19,24 @@ function Map($scope, $http, GeolocationService, SpeechService, Focus) {
 			targetZoom = map.zoom;
 		}
 		return "https://maps.googleapis.com/maps/api/staticmap?center=" + targetCenter + "&zoom=" + targetZoom +
-            "&format=png&sensor=false&scale=2&size=" + window.innerWidth +
-            "x1200&maptype=roadmap&style=visibility:on|weight:1|invert_lightness:true|saturation:-100|lightness:1";
+            "&size=" + window.innerWidth + "x" + window.innerHeight +
+            "&key=" + config.youtube.key;
+	};
+
+	var address = function (station){// 내가 생각하는 해결책은 여기에 아래 만든 번역 함수를 집어넣어 
+		var data;
+		var temp;
+        //var station = decodeURI(station);    //이걸 써도 안먹힌다.
+		var url ="https://maps.googleapis.com/maps/api/geocode/json?&address="+station+"&key="+config.youtube.key;
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, false); //true:비동기 false:동기
+		xhr.onreadystatechange = function() {
+			data = xhr.responseText;
+			temp = JSON.parse(data);
+			console.log(temp);
+		};
+		xhr.send();
+		return temp;
 	};
 
     // Show map
@@ -31,7 +47,9 @@ function Map($scope, $http, GeolocationService, SpeechService, Focus) {
 
     // Hide everything and "sleep"
 	SpeechService.addCommand('map_location', function (location) {
-		$scope.map = generateMap(location);
+		var loca = address(location);
+		map.center = loca.results[0].geometry.location.lat + ',' + loca.results[0].geometry.location.lng;
+		$scope.map = generateMap(map.center,map.zoom);
 		Focus.change("map");
 	});
 
@@ -47,7 +65,7 @@ function Map($scope, $http, GeolocationService, SpeechService, Focus) {
 	});
 
 	SpeechService.addCommand('map_zoom_reset', function () {
-		map.zoom = 13;
+		map.zoom = 16;
 		$scope.map = generateMap();
 	});
 
